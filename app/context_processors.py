7ab17cc3fb361c8e3aa69bd9e28f90bc48c7152f
Diego Federico Bravo <fedebravo2016@gmail.com>
2024-04-29 14:35:59 -0300

    agrego opcion medicinas tanto para el navbar como para el home

--- app/context_processors.py
+++ app/context_processors.py
@@ -4,6 +4,7 @@ links = [
     {"label": "Home", "href": reverse("home"), "icon": "bi bi-house-door"},
     {"label": "Clientes", "href": reverse("clients_repo"), "icon": "bi bi-people"},
     {"label": "Veterinarios", "href": reverse("vets_repo"), "icon": "bi bi-bag"},
+    {"label": "Medicinas", "href": reverse("medicines_repo"), "icon": "bi bi-capsule"},
 ]
 
 
