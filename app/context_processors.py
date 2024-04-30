dc22d215131590d3c83aa5b9871e9c7d64e390e4
brunolopezv <90809331+brunolopezv@users.noreply.github.com>
2024-04-30 18:34:47 -0300

    Agrego Productos a la navbar

--- app/context_processors.py
+++ app/context_processors.py
@@ -4,6 +4,7 @@ links = [
     {"label": "Home", "href": reverse("home"), "icon": "bi bi-house-door"},
     {"label": "Clientes", "href": reverse("clients_repo"), "icon": "bi bi-people"},
     {"label": "Veterinarios", "href": reverse("vets_repo"), "icon": "bi bi-bag"},
+    {"label": "Productos", "href": reverse("products_repo"), "icon": "bi bi-box-seam"},
 ]
 
 
