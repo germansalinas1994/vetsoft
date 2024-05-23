6ac475ef07f42f0bd5b3b9a56b6ab6a3b5f2acd4
brunolopezv <90809331+brunolopezv@users.noreply.github.com>
2024-05-23 15:07:34 -0300

    Agrego test unitarios para Product

--- app/tests_unit.py
+++ app/tests_unit.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from app.models import Client
-
+from app.models import Product
+from decimal import Decimal
 
 class ClientModelTest(TestCase):
     def test_can_create_and_get_client(self):
@@ -57,3 +58,78 @@ class ClientModelTest(TestCase):
         client_updated = Client.objects.get(pk=1)
 
         self.assertEqual(client_updated.phone, "221555232")
+
+
+# PRODUCT
+class ProductModelTest(TestCase):
+    def test_can_create_and_get_product(self):
+        Product.save_product(
+            {
+                "name": "Whiskas",
+                "type": "Gato adulto",
+                "price": "1454.3",
+            }
+        )
+        products = Product.objects.all()
+        self.assertEqual(len(products), 1)
+
+        self.assertEqual(products[0].name, "Whiskas")
+        self.assertEqual(products[0].type, "Gato adulto")
+        self.assertEqual(products[0].price, 1454.3)
+
+    def test_can_update_product(self):
+        Product.save_product(
+            {
+                "name": "Whiskas",
+                "type": "Gato adulto",
+                "price": "1454.3",
+            }
+        )
+        product = Product.objects.get(pk=1)
+
+        self.assertEqual(product.price, 1454.3)
+
+        product.update_product({"price": 1454.3})
+
+        product_updated = Product.objects.get(pk=1)
+
+        self.assertEqual(product_updated.price, 1454.3)
+
+    def test_update_product_with_error(self):
+        Product.save_product(
+            {
+                "name": "Whiskas",
+                "type": "Gato adulto",
+                "price": "1454.3",
+            }
+        )
+        product = Product.objects.get(pk=1)
+
+        self.assertEqual(product.price, 1454.3)
+
+        product.update_product({"name": "Whiskas", "type": "Gato adulto","price": ""})
+
+        product_updated = Product.objects.get(pk=1)
+
+        self.assertEqual(product_updated.price, 1454.3)
+
+    def test_product_price_no_negative(self):
+        valid, errors = Product.save_product({
+            "name": "DogChow",
+            "type": "Perro adulto",
+            "price": "-434.00"
+        })
+        self.assertFalse(valid)
+        self.assertIn("price", errors)
+        self.assertEqual(errors["price"], "Por favor ingrese un precio positivo")
+
+
+    def test_product_price_no_words_or_symbols(self):
+        valid, errors = Product.save_product({
+            "name": "DogChow",
+            "type": "Perro adulto",
+            "price": "-434abc/e"
+        })
+        self.assertFalse(valid)  
+        self.assertIn("price", errors)  
+        self.assertEqual(errors["price"], "Por favor ingrese un precio válido") 
